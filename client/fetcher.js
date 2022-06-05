58ebb756f5313e7cfa7d1256e92418ed54299660
suyeon <suyeon.dv@gmail.com>
2022-06-05 16:49:23 +0900

    [FEAT] client graphql 작업 추가

--- client/fetcher.js
+++ client/fetcher.js
@@ -1,19 +1,27 @@
-import axios from 'axios';
+// fetcher.js
+// import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000';
+// axios.defaults.baseURL = 'http://localhost:8000';
 
-const fetcher = async (method, url, ...rest) => {
-    const res = await axios[method](url, ...rest);
-    return res.data;
-}
+// const fetcher = async (method, url, ...rest) => {
+//     const res = await axios[method](url, ...rest);
+//     return res.data;
+// }
 
-export default fetcher;
+// export default fetcher;
 
 /*
 get: axios.get(url[, config])
 delete: axios.delete(url[, config])
 post: axios.post(url, data[, config]);
 put: axios.put(url, data[, config]);
-*/ 
+*/
 
 // post, put의 경우 data인자가 추가로 들어와야 하기 때문에 ...rest로 함수 만듬
+
+// queryClient.js
+import { request } from "graphql-request";
+import fetcher from "./fetcher";
+const URL = "http://localhost:8000/graphql";
+
+export const fetcher = (query, variables = {}) => request(URL, query, variables);
